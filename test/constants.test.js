99dcffe913bf47443b0b5baf86d146f294b00fb6
mangezi <ngeziman@live.com>
2026-07-30 13:41:15 +0200

    Add direct TV web controls (#4)

---
+++ test/constants.test.js
@@ -0,0 +1,17 @@
+import { test } from 'node:test';
+import assert from 'node:assert/strict';
+import {
+  FEATURED_OFFICIAL_SERVICE_IDS,
+  OFFICIAL_SERVICES,
+} from '../src/constants.js';
+
+test('featured official services keep the requested direct-button order and labels', () => {
+  const serviceById = Object.fromEntries(OFFICIAL_SERVICES.map((service) => [service.id, service]));
+  const featured = FEATURED_OFFICIAL_SERVICE_IDS.map((id) => serviceById[id]);
+
+  assert.deepEqual(
+    featured.map((service) => service.shortLabel),
+    ['AfreeTV', 'e+', 'SABC+', 'Z+'],
+  );
+  assert.equal(featured.every((service) => service.url.startsWith('https://')), true);
+});
